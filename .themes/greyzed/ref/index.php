d6d06a890dab4e75255db7c5fe4da0b6cfc580c5
Fred Ghilini <frederic.ghilini@groupon.com>
2013-10-15 15:00:55 +0200

    Continued template conversion for Greyzed

--- .themes/greyzed/ref/index.php
+++ .themes/greyzed/ref/index.php
@@ -6,43 +6,43 @@
 get_header(); ?>
 
 <div id="container">	
-<?php get_sidebar(); ?>	
+	<?php get_sidebar(); ?>	
 	<div id="content" role="main">
 
-	
-	<div class="column">
-	<?php $postcount = "0"; // reset post counter ?>
-	<?php if (have_posts()) : ?>
+		
+		<div class="column">
+			<?php $postcount = "0"; // reset post counter ?>
+			<?php if (have_posts()) : ?>
 
-		<?php while (have_posts()) : the_post(); ?>
-          <?php $postcount++; // post counter ?>
+			<?php while (have_posts()) : the_post(); ?>
+			<?php $postcount++; // post counter ?>
 			<div <?php post_class() ?> id="post-<?php the_ID(); ?>">
 				<div class="posttitle">
 					<h2 class="pagetitle"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
-				<small>Posted: <?php the_time('jS F Y') ?> by <strong><?php the_author() ?></strong> in <?php the_category(', ') ?><br />
-				<?php the_tags( 'Tags: ', ', ', ''); ?></small>
-				</div>
-				<div class="postcomments"><?php comments_popup_link('0', '1', '%'); ?></div>
+					<small>Posted: <?php the_time('jS F Y') ?> by <strong><?php the_author() ?></strong> in <?php the_category(', ') ?><br />
+						<?php the_tags( 'Tags: ', ', ', ''); ?></small>
+					</div>
+					<div class="postcomments"><?php comments_popup_link('0', '1', '%'); ?></div>
 
-				<div class="entry">
-					<?php the_content('Read the rest of this entry &raquo;'); ?>
+					<div class="entry">
+						<?php the_content('Read the rest of this entry &raquo;'); ?>
+					</div>
+					
+					<?php wp_link_pages(array('before' => '<p><strong>Pages:</strong> ', 'after' => '</p>', 'next_or_number' => 'number')); ?>
+					
 				</div>
-				
-				<?php wp_link_pages(array('before' => '<p><strong>Pages:</strong> ', 'after' => '</p>', 'next_or_number' => 'number')); ?>
-				
-			</div>
-		<?php endwhile; ?>
+			<?php endwhile; ?>
 
-	<?php
+			<?php
 	// Find page with last post
-	$paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
-	$postsppage= get_option('posts_per_page');
-	$total = $paged * $postsppage;
-	$remainder = $total - $wp_query->found_posts;
-	$endvar =  $postsppage - $remainder;
-	?>	
+			$paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+			$postsppage= get_option('posts_per_page');
+			$total = $paged * $postsppage;
+			$remainder = $total - $wp_query->found_posts;
+			$endvar =  $postsppage - $remainder;
+			?>	
 
-	</div>
+		</div>
 		<div id="nav-post">
 			<div class="navigation-bott">
 				<?php if($endvar == 0 || $postcount == $endvar) { } else { ?>
